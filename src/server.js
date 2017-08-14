fe060c56088fa9bec6a480c5b301191de62d6644
Band Ly <bandly@Bands-MacBook-Pro.local>
2017-08-14 15:02:10 -0700

    connected mongodb and added user schemas

--- src/server.js
+++ src/server.js
@@ -3,17 +3,25 @@ var bodyParser = require("body-parser");
 var engines = require('consolidate');
 var app = express();
 var path = require("path");
+var mongoose = require("mongoose");
+
+var URI = "mongodb://localhost:27017/tesing_db"
+
+mongoose.connect(URI, function(err, db) {
+  if(!err) {
+    console.log("You are now connected to the mongo database!");
+  }else{
+  	console.log(err);
+  }
+});
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.engine('html', engines.mustache);
 app.set("view engine", "html");
 
-//app.use(express.static(__dirname + '/dist'));
 app.use(express.static(path.join(__dirname, '../dist')));
 app.use('/',express.static(__dirname + '/views'));
 
-
-console.log(__dirname);
 app.get("/goodbye",function(req,res){
 	res.send("goodbye");
 });
